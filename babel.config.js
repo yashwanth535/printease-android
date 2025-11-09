74e609911ea4605f615e4ef2a95e46a6930b4443
yashwanth535 <yashwanth.lumia535@gmail.com>
2025-11-09 18:30:58 +0530

    for apk bundle changes in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,27 @@
 module.exports = function (api) {
   api.cache(true);
-  const plugins = [
-    'react-native-worklets/plugin',
-    [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-        blacklist: null,
-        whitelist: null,
-        safe: false,
-        allowUndefined: true,
-      },
-    ],
-  ];
-
 
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-    plugins,
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel'
+    ],
+    plugins: [
+      'react-native-worklets/plugin',
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true
+        }
+      ],
+
+      // ✅ This must always be LAST
+      'react-native-reanimated/plugin'
+    ]
   };
 };
