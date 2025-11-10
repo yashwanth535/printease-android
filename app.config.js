5c7cc6b99fca1cba2b2b76a3426166c111e1e322
yashwanth535 <yashwanth.lumia535@gmail.com>
2025-11-10 17:15:39 +0530

    changes in app.config.js

--- app.config.js
+++ app.config.js
@@ -5,6 +5,9 @@ export default {
     name: "PrintEase",
     slug: "PrintEase",
     version: "1.0.0",
+    android: {
+      package: "com.yashwanth535.PrintEase",
+    },
     extra: {
       API_URL: process.env.API_URL,
     },
